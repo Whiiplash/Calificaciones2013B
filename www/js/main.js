b16925aff99a786b630071afb7afcef8ca8deced
Omar Alejandro Valencia Macias <omar.val2189@gmail.com>
2013-12-01 18:56:05 -0800

    Jquery, css y altas
    inse4rcion de jquery, modificaciones a clases de css y al html de las
    altas

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,39 @@
+function validacion(){
+/*****************************
+alta usuario
+*****************************/
+	if($('#nombre').val()=='')
+		alert("hola")
+	if($('#apellidop').val()=='')
+		alert('nombre')
+	if($('#apellidom').val()=='')
+		alert("hola")
+	if($('#carreras').val()=='')
+		alert("hola")
+	if($('#email').val()=='')
+		alert("hola")
+	if($('#celular').val()=='')
+		alert("hola")
+	if($('#github').val()=='')
+		alert("hola")
+	if($('#web').val()=='')
+		alert("hola")
+/*****************************
+alta curso
+*****************************/
+$('#carreras')
+$('#nombreCurso')
+$('#seccion')
+$('#nrc')
+$('#academia')
+$('#lunes')
+$('#martes')
+$('#miercoles')
+$('#jueves')
+$('#viernes')
+$('#horas')
+$('#horario')
 
+
+
+}
